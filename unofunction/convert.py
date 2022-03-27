3f8eea4398a5bd1e00e8e9598562b4dc3fe45520
Javid Lakha <javid@javidlakha.com>
2022-03-27 22:23:45 +0100

    Unofunction lambda (v0.1.0)

---
+++ unofunction/convert.py
@@ -0,0 +1,92 @@
+import logging
+import os
+import subprocess
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+LIBREOFFICE_PATH = os.environ['LIBREOFFICE_PATH']
+
+
+class ConversionError(Exception):
+    pass
+
+
+def convert_file(
+    filepath: str,
+    outdir: str,
+    convert_to: str,
+    num_attempts: int,
+) -> None:
+    """
+    Calls LibreOffice to convert `filepath` to the output type specified in 
+    `convert_to`.
+
+    :param filepath: the path to the input file
+    :param outdir: the path to the output directory
+    :param convert_to: the file format of the converted type
+    :param num_attempts: number of conversion attempts to make
+    :raise ConversionError: if the conversion cannot be performed
+    """
+    commands = [
+        LIBREOFFICE_PATH,
+
+        # Without this, LibreOffice will return 'Fatal Error: The application
+        # cannot be started. User installation could not be completed' because
+        # AWS Lambda invocations do not have write access to
+        # ~/.config/libreoffice
+        '-env:UserInstallation=file:///tmp/'
+
+        '--headless',
+        '--invisible',
+        '--nodefault',
+        '--nofirststartwizard',
+        '--nolockcheck',
+        '--nologo',
+        '--norestore',
+        '--writer',
+
+        '--convert-to',
+        convert_to,
+
+        '--outdir',
+        outdir,
+
+        filepath
+    ]
+
+    filepath_root, _ = os.path.splitext(filepath)
+    expected_outpath = f'{filepath_root}.{convert_to}'
+
+    # On a cold start, LibreOffice often requires several attempts to convert
+    # a file before it succeeds
+    for attempt in range(num_attempts):
+        response = subprocess.run(
+            commands,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        stdout = response.stdout.decode('utf8').strip()
+        stderr = response.stderr.decode('utf8').strip()
+
+        if response.returncode == 0 and os.path.exists(expected_outpath):
+            logger.info(
+                'Conversion successful on attempt '
+                f'{attempt+1}/{num_attempts}. '
+                f'stdout: "{stdout}". stderr: "{stderr}".'
+            )
+            break
+
+        logger.info(
+            f'Conversion attempt {attempt+1}/{num_attempts} failed. '
+            f'stdout: "{stdout}". stderr: "{stderr}".'
+        )
+
+    # If all attempts fail, raise an error and report the output and error
+    # messages from the last attempt
+    else:
+        raise ConversionError(
+            'Unable to convert file. On the last attempt '
+            f'({attempt+1}/{num_attempts}) the output was as follows - '
+            f'stdout: "{stdout}". stderr: "{stderr}".'
+        )
---
+++ unofunction/handler.py
@@ -0,0 +1,85 @@
+import logging
+import os
+import tempfile
+
+import awslambdaric
+import boto3
+
+from convert import convert_file
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+DEFAULT_ATTEMPTS = 3
+
+s3 = boto3.client('s3')
+
+
+class ParameterError(Exception):
+    pass
+
+
+def handler(
+        event: dict[str, str], 
+        context: awslambdaric.lambda_context.LambdaContext,
+    ) -> None:
+    """
+    Handles file download, conversion and upload
+
+    :param event: function invocation parameters
+    - input_bucket: the bucket from which to download the file to be converted
+    - input_path: the path to the file to be converted in `input_bucket`
+    - output_bucket: the bucket to upload the converted file
+    - output_path: the path to upload the converted file in `output_bucket`
+    - convert_to (optional): the file format of the converted type. Must be 
+      specified if `output_path` does not have a file extension
+    - num_attempts: number of conversion attempts to make. If not specified, 
+      defaults to `DEFAULT_ATTEMPTS`
+
+    :param context: AWS Lambda context object
+    
+    :raise ParameterError: if 'convert_to' is not specified in `event` and 
+    the output file format cannot be inferred from 'output_path'
+    """
+    input_bucket = event['input_bucket']
+    input_path = event['input_path']
+    num_attempts = event.get('num_attempts', DEFAULT_ATTEMPTS)
+    output_bucket = event['output_bucket']
+    output_path = event['output_path']
+
+    # If 'convert_to' is specified in `event` then use that. Otherwise,
+    # attempt to infer it from `output_path`
+    if 'convert_to' in event:
+        convert_to = event['convert_to']
+    else:
+        _, output_extension = os.path.splitext(output_path)
+        if output_extension:
+            logger.info(
+                'convert_to not specified. '
+                f'Inferring type "{output_extension}" from output_path '
+                f'"{output_path}".'
+            )
+            convert_to = output_extension
+        else:
+            raise ParameterError(
+                'Target file type could not be inferred from output_path '
+                f'"{output_path}". Please specify convert_to.'
+            )
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        _, input_path_tail = os.path.split(input_path)
+        download_path = f'{temp_dir}/{input_path_tail}'
+        s3.download_file(input_bucket, input_path, download_path)
+        logger.info(
+            f'Successfully downloaded "s3://{input_bucket}/{input_path}".'
+        )
+
+        convert_file(download_path, temp_dir, convert_to, num_attempts)
+
+        input_path_root, _ = os.path.splitext(input_path_tail)
+        upload_path = f'{temp_dir}/{input_path_root}.{convert_to}'
+        s3.upload_file(upload_path, output_bucket, output_path)
+        logger.info(
+            'Successfully uploaded converted document to '
+            f'"s3://{output_bucket}/{output_path}".'
+        )
