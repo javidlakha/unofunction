93f4984b4d9ccd12df43d86c639c0f4275f7e0f9
Javid Lakha <javid@javidlakha.com>
2022-04-09 23:16:39 +0100

    AWS SAM deployment example

--- deployment-examples/cdk/deploy.py
+++ deployment-examples/cdk/deploy.py
@@ -1,3 +1,17 @@
+"""
+Deploys Unofunction and creates an S3 bucket that Unofunction can read from
+and write to
+
+To install the AWS CDK, see 
+https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html
+
+To deploy this stack, run
+    cdk deploy
+
+To tear down this stack, run
+    cdk destroy
+Note that this will fail if the S3 bucket is not first emptied
+"""
 from aws_cdk import core as cdk
 from aws_cdk import aws_lambda
 from aws_cdk import aws_s3 as s3
@@ -9,16 +23,17 @@ class Unofunction(cdk.Stack):
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Lambdas
+        # Lambda
         self.convert = aws_lambda.DockerImageFunction(
             code=aws_lambda.DockerImageCode.from_image_asset(
                 cmd=['handler.handler'],
-                directory='../unofunction',
+                directory='../../unofunction',
             ),
             id='unofunction',
             function_name='unofunction',
 
-            # Larger instances run on faster processes
+            # Must be at least 256 (megabytes). Larger instances run on faster
+            # processors and hence convert files quicker
             memory_size=1024,
 
             scope=self,
@@ -28,7 +43,7 @@ class Unofunction(cdk.Stack):
             timeout=cdk.Duration.seconds(300),
         )
 
-        # Storage
+        # S3
         self.storage = s3.Bucket(
             access_control=s3.BucketAccessControl.PRIVATE,
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
